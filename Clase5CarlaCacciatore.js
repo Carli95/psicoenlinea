3b7af6f1cee6338e1b5a5fbe7c8df65d7f52cb4c
Carli Cacciatore <carlantonella55@gmail.com>
2021-09-13 19:08:38 -0300

    Desafio 5 Objetos

---
+++ Clase5CarlaCacciatore.js
@@ -0,0 +1,47 @@
+class turnoConfirmado {
+
+    constructor(nombre, apellido, email, horario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.horario = horario;
+        this.reservado = false;
+    }
+
+    datosTurno() {
+
+        if (!this.nombre) {
+            alert("El turno no puede ser reservado sin los datos");
+            return false;
+        }
+
+        if (!this.apellido) {
+            alert("El turno no puede ser reservado sin los datos");
+            return false;
+        }
+
+        if (!this.email) {
+            alert("El turno no puede ser reservado sin los datos");
+            return false;
+        }
+
+        if (!this.horario) {
+            alert("El turno no puede ser reservado sin el horario");
+            return false;
+        }
+
+        this.reservado = true;
+        return "Gracias" + " " + this.nombre + " "+  this.apellido + " " + "su turno fue reservado";
+    }
+
+}
+
+let nombre = prompt("Para agendar tu turno dejanos tu nombre");
+let apellido = prompt("Tu apellido tambien");
+let email = prompt("Tu email para notificarte novedades sobre tu turno");
+let horario = prompt("Tu horario de preferencia, indicando si es por la mañana o tarde");
+
+const turno1 = new turnoConfirmado(nombre, apellido, email, horario);
+
+
+console.log(turno1.datosTurno());
