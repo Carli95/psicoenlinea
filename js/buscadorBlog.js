e21578ffb8322f0e8b5a02fff767dd70cb1091c0
Carli Cacciatore <carlantonella55@gmail.com>
2021-10-13 22:03:59 -0300

    desafio animacion

--- js/buscadorBlog.js
+++ js/buscadorBlog.js
@@ -8,16 +8,3 @@ $(document).ready(function(){
   });
 
 
-
-
-/*const Articulos = [{nombre:"¿Cómo ayudar a alguien con depresion?",},{nombre:"¿Que es la ansiedad?"},{nombre:"ESI Qué es?"},{nombre:"Conoce más sobre el estres laboral"},{nombre:"¿Que es la autoexigencia?"},{nombre:"Tips para el insomnio"}];
-
-document.getElementById("buscador").addEventListener("submit", (e) => {
-
-    e.preventDefault();
-    const termino = $("#termino");
-    const arrayArticulos = Articulos.filter(articulo => articulo.nombre.includes(termino))
-    console.log(arrayArticulos);
-    });
-
-*/
